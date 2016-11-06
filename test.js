11db67451053c841c6034fa73044bfe886e8e666
David Knoll <david@davidknoll.me.uk>
2016-11-06 02:12:34 +0000

    Look up some information in the test junk file

--- test.js
+++ test.js
@@ -8,13 +8,32 @@
 // Builtin imports
 const fs = require('fs');
 // Project imports
-const ff7lib = require('./ff7lib');
+const FF7Lib = require('./ff7lib');
 
+// Load a file
 const rawbuf = fs.readFileSync(process.argv[2]);
-const savefile = new ff7lib(rawbuf);
+const savefile = new FF7Lib(rawbuf);
 
-const zz = savefile.get('saves').get(0).get('preview').get('location').defftext();
-console.log(zz);
+// Look at some details
+const leader = savefile
+  .get('saves').get(0).get('preview').get('name')
+  .defftext();
+const location = savefile
+  .get('saves').get(0).get('preview').get('location')
+  .defftext();
+const gil = savefile
+  .get('saves').get(0).get('preview').get('gil');
 
-const blank = new ff7lib();
-console.log(blank.buffer().length);
+console.log(`Leader is ${leader}`);
+console.log(`Location is ${location}`);
+console.log(`You have ${gil} gil`);
+
+const party = savefile.get('saves').get(0).get('party-members');
+console.log('In party:');
+for (let i = 0; i < party.length(); i++) {
+  const orig = FF7Lib.enum.character[party.get(i)];
+  const chosen = savefile
+    .get('saves').get(0).get('character-records')
+    .get(party.get(i)).get('name').defftext();
+  console.log(`Original: ${orig} Chosen: ${chosen}`);
+}
